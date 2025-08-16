bc64afe9fa47d343727462b54b030c0a4bde2c2a
Alif-Sarker <sarker202454@gmil.com>
2025-08-16 16:37:23 +0600

    remove  the token veification from the details page

--- index.js
+++ index.js
@@ -483,7 +483,7 @@ app.get("/products",verifyFirebaseToken, async (req, res) => {
 
 // get single product  by id
 
-app.get("/productDetails/:id",verifyFirebaseToken, async(req,res) =>{
+app.get("/productDetails/:id", async(req,res) =>{
     const id = req.params.id;
     const query = {_id: new ObjectId(id)}
     const result = await productCollection.findOne(query)
